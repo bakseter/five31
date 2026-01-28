ffa0f991e5bce097b93d0ff4d18d27485c15439c
Andreas Salhus Bakseter <6720179+bakseter@users.noreply.github.com>
2026-01-28 23:07:32 +0100

    Upgrade backend + CI/CD deps

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -9,10 +9,10 @@ val flywayVersion: String by project
 
 plugins {
     application
-    kotlin("jvm") version "2.2.21"
-    kotlin("plugin.serialization") version "2.2.21"
-    id("com.gradleup.shadow") version "8.3.9"
-    id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
+    kotlin("jvm") version "2.3.0"
+    kotlin("plugin.serialization") version "2.3.0"
+    id("com.gradleup.shadow") version "9.3.1"
+    id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
     id("com.adarshr.test-logger") version "4.0.0"
 }
 
