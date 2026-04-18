a6441838d3d69b69b077aa870729c0dcb5ecda70
Andreas Salhus Bakseter <6720179+bakseter@users.noreply.github.com>
2026-04-18 14:58:29 +0200

    Remove next-pa & upgrade deps

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,5 @@
-import withPWAInit from '@ducanh2912/next-pwa';
-
-const withPWA = withPWAInit({
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-});
-
-export default withPWA({
+export default {
     reactStrictMode: true,
     output: 'standalone',
     turbopack: {},
-});
+};
